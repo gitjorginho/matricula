55f746e013ea36c6fea7e66a860f1646f1b8850a
gitjorginho <40150765+gitjorginho@users.noreply.github.com>
2021-01-14 14:37:21 -0300

    salvado edicao
    Salva dados  e audiria

--- registro_update.php
+++ registro_update.php
@@ -14,6 +14,7 @@ $sexo_aluno         =$_POST['vch_sexo'];
 $data_nascimento    = dateToDatabase($_POST['sdt_nascimento']);
 $nome_mae           =$_POST['vch_mae'];
 $nome_responsavel   =$_POST['vch_responsavel'];
+$email_responsavel   =$_POST['vch_email_responsavel'];
 $cpf_responsavel    =$_POST['vch_cpf'];
 $endereco           =$_POST['vch_endereco'];
 $bairro             =$_POST['vch_bairro'];
@@ -23,11 +24,10 @@ $cep                =$_POST['vch_cep'];
 $cidade             =$_POST['vch_cidade'];
 $serie              =$_POST['vch_serie'];
 $escola             =$_POST['escola'];
-$codigo_localidade  =$_POST['vch_localidade'];
-$cpf_responsavel    = str_replace(['.','-'],'',$cpf_responsavel);
 $numero             =$_POST['vch_numero'];
+$acoes              =$_POST['vch_acoes'];
+
 $_SESSION['vch_serie'] = $serie;
-$cpf_responsavel = str_replace(['.','-'],'',$cpf_responsavel);
 $telefone = str_replace(['(',')','-'],'',$telefone);
 $telefone = trim($telefone);
 
@@ -38,20 +38,46 @@ $telefone = trim($telefone);
 //		Nascimento: $data_nascimento - Localidade: $codigo_localidade - Telefone: $telefone -
 //		Numero: $numero - Cpf responsavel: $cpf_responsavel','now', $codigo_aluno);";
 //		$monitoramentoreserva = pg_query($conn, $sql_insert_monitoramentomatriculareserva);
+		//ed47_i_localidade=$codigo_localidade
 		
+		//salva alteracao aluno
 		$sql_update_aluno ="
-		UPDATE reserva.alunoreserva SET ed47_c_numero ='$numero',ed47_v_nome='$nome_aluno', ed47_v_telef='$telefone' , ed47_v_ender='$endereco',ed47_v_bairro='$bairro',ed47_v_cep='$cep',ed47_c_nomeresp='$nome_responsavel',
-		ed47_v_mae='$nome_mae',ed47_v_sexo='$sexo_aluno',ed47_d_nasc='$data_nascimento',municipio='$cidade',ed47_i_localidade=$codigo_localidade
+		UPDATE reserva.alunoreserva SET 
+		ed47_c_numero ='$numero',
+		ed47_v_nome='$nome_aluno', 
+		ed47_v_telef='$telefone' , 
+		ed47_v_ender='$endereco',
+		ed47_v_bairro='$bairro',
+		ed47_v_cep='$cep',
+		ed47_c_nomeresp='$nome_responsavel',
+		ed47_v_mae='$nome_mae',
+		ed47_v_sexo='$sexo_aluno',
+		ed47_d_nasc='$data_nascimento',
+		municipio='$cidade', 
+		email_resp='$email_responsavel',
+		ed47_v_cpf='$cpf_responsavel',
+		ed47_i_localidade='$localidade' 
 		WHERE id_alunoreserva = $codigo_aluno ";
 		$result = pg_query($conn,$sql_update_aluno);
-
+		 
+		
+		//salva a alteracao da escola
 		$sql_update_escola = "update reserva.escolareserva set ed56_i_escola = $escola, ed221_i_serie = $serie where id_alunoreserva = $codigo_aluno";
         $result = pg_query($conn,$sql_update_escola);
+
+		//salva auditoria  
+		$sql_auditoria = "INSERT INTO reserva.auditoriausuarioaluno
+		(nome_usuario, id_alunoreserva, descricao, data_modificacao, acoes)
+		VALUES('PROPRIO ALUNO',$codigo_aluno, 'atulizar dados pelo proprio aluno', now(), '$acoes')";
+		
+		
+		$result = pg_query($conn,$sql_auditoria);
+            
+
 
 
         // chama a pagina de comprovante
                 $_SESSION['vch_nome'] = trim($nome_aluno);
-				//$_SESSION['turma'] = $turma;
 				$_SESSION['escola'] = $escola;
 				header('Location:comprovante.php');
 
