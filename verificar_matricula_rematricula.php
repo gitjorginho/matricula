36eadfaacb1fbc0417c274556714268ebed58702
JoelJCL <joel.portugual@jcl-tecnologia.com.br>
2021-01-22 11:29:23 -0300

    Ultima etapa da escola

--- verificar_matricula_rematricula.php
+++ verificar_matricula_rematricula.php
@@ -27,7 +27,7 @@ if (isset($_POST['vch_nome_aluno'])) {
 
 if ($cod_aluno != ''){
 	$sql_matricula = "
-    select (select true from confirmacaorematricula where edu01_aluno = ed47_i_codigo) as confirmacao_rematricula, reserva.alunoreserva.*, ap.ed79_i_serie,ac.ed56_i_escola from reserva.alunoreserva left join escola.alunocurso ac on ed47_i_codigo = ac.ed56_i_aluno join escola.alunopossib ap on ac.ed56_i_codigo = ap.ed79_i_alunocurso
+    select (select true from confirmacaorematricula where edu01_aluno = ed47_i_codigo) as confirmacao_rematricula, reserva.alunoreserva.*, ap.ed79_i_serie as idserie,ac.ed56_i_escola as idescola, ac.ed56_i_calendario as idcalendario, ac.ed56_i_base as idbase from reserva.alunoreserva left join escola.alunocurso ac on ed47_i_codigo = ac.ed56_i_aluno join escola.alunopossib ap on ac.ed56_i_codigo = ap.ed79_i_alunocurso
     where ed47_i_codigo  = '$cod_aluno' limit 1 ";
 
     $result = pg_query($conn, $sql_matricula);
@@ -37,15 +37,41 @@ if ($cod_aluno != ''){
         header('Location:index.php?rematricula=1');
     }
     else{
-        if (pg_num_rows($result) >= 1) {
-            $_SESSION['codigo'] = $aluno['id_alunoreserva'];
-            $_SESSION['matriculado'] = 'false';
-            $_SESSION['escola'] = 'true';
-            header('Location:rematricula_update.php');
+        $sql_etapaescola = "
+        select
+            s2.ed11_i_codigo as idserie
+        from
+            escola.turma t,
+            escola.turmaserieregimemat t2,
+            escola.serieregimemat s,
+            escola.serie s2
+        where
+            t.ed57_i_escola = {$aluno['idescola']}
+            and t.ed57_i_calendario = {$aluno['idcalendario']}
+            and t.ed57_i_base = {$aluno['idbase']}
+            and t2.ed220_i_turma = t.ed57_i_codigo
+            and t2.ed220_i_serieregimemat = s.ed223_i_codigo
+            and s.ed223_i_serie = s2.ed11_i_codigo
+        order by
+            s2.ed11_c_descr desc
+        limit 1;";
+    
+        $result = pg_query($conn, $sql_etapaescola);
+        $etapaescola = pg_fetch_assoc($result);
+        if($aluno['idserie'] == $etapaescola['idserie']){
+            header('Location:index.php?ultimaetapa=1');
         }
-        else {
-            header('Location:index.php?not_found=1');
-        }
+        else{
+            if (pg_num_rows($result) >= 1) {
+                $_SESSION['codigo'] = $aluno['id_alunoreserva'];
+                $_SESSION['matriculado'] = 'false';
+                $_SESSION['escola'] = 'true';
+                header('Location:rematricula_update.php');
+            }
+            else {
+                header('Location:index.php?not_found=1');
+            }
+        }   
     }
 }
 else{
